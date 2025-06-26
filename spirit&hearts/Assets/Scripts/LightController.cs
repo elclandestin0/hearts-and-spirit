7bb1fe24a5389db26c9703b431c9a944021667b0
Memo Khoury <majd.khoury93@gmail.com>
2025-06-26 13:40:23 +0100

    Lit lighthouse

--- spirit&hearts/Assets/Scripts/LightController.cs
+++ spirit&hearts/Assets/Scripts/LightController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class LightController : MonoBehaviour
 {
-    public Renderer lanternRenderer;
+    public Material lightMaterial;
 
     [Header("Lighting Settings")]
     public Color unlitColor;
@@ -16,16 +16,28 @@ public class LightController : MonoBehaviour
 
     [Header("State")]
     public bool isLit = false;
-
-    private Material _mat => lanternRenderer.material;
     private bool _previousLitState;
+    private Material _mat
+    {
+        get
+        {
+            var mats = GetComponent<Renderer>().materials;
+            foreach (var m in mats)
+            {
+                if (m.name.Contains("WindowEmi"))
+                    return m;
+            }
+            return null;
+        }
+    }
+
 
     private void Start()
     {
-        lanternRenderer.material = new Material(lanternRenderer.material);
         _previousLitState = isLit;
         ApplyInitialState();
     }
+
 
     private void Update()
     {
@@ -60,16 +72,24 @@ public class LightController : MonoBehaviour
         float targetIntensity = toLit ? litIntensity : unlitIntensity;
 
         float time = 0f;
+        _mat.EnableKeyword("_EMISSION");
+
         while (time < transitionDuration)
         {
             float t = time / transitionDuration;
-            Color blended = Color.Lerp(startColor, targetColor * targetIntensity, t);
-            _mat.SetColor("_BaseColor", blended);
+            Color baseBlended = Color.Lerp(startColor, targetColor * targetIntensity, t);
+            Color emissionBlended = Color.Lerp(unlitColor, litColor, t) * Mathf.Lerp(unlitIntensity, litIntensity, t);
+
+            _mat.SetColor("_BaseColor", baseBlended);
+            _mat.SetColor("_EmissionColor", emissionBlended);
+
             time += Time.deltaTime;
             yield return null;
         }
 
         _mat.SetColor("_BaseColor", targetColor * targetIntensity);
+        _mat.SetColor("_EmissionColor", targetColor * targetIntensity);
+
     }
 
     private float GetIntensity(Color c) => Mathf.Max(c.r, c.g, c.b);
