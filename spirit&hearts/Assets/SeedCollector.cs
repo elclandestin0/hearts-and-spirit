1f903cbdf4770e041fc175a4b45725282ac7c64a
Memo Khoury <majd.khoury93@gmail.com>
2025-07-01 15:11:43 +0100

    updates

--- spirit&hearts/Assets/SeedCollector.cs
+++ spirit&hearts/Assets/SeedCollector.cs
@@ -12,5 +12,16 @@ public class SeedCollector : MonoBehaviour
             Destroy(other.gameObject);
             Debug.Log("Seed collected! Total: " + seedsCollected);
         }
+
+        if (other.CompareTag("Light") && seedsCollected > 0)
+        {
+            LightController light = other.GetComponent<LightController>();
+            if (light != null && !light.isLit)
+            {
+                light.isLit = true;
+                seedsCollected--;
+                Debug.Log("Seed used to light a source! Seeds remaining: " + seedsCollected);
+            }
+        }
     }
 }
