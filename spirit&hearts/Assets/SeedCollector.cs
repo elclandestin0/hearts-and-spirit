d9d5c733cfab15b2c060ea300dd0284a55b581a9
Memo Khoury <majd.khoury93@gmail.com>
2025-07-02 15:29:34 +0100

    audio playing

--- spirit&hearts/Assets/SeedCollector.cs
+++ spirit&hearts/Assets/SeedCollector.cs
@@ -4,30 +4,33 @@ public class SeedCollector : MonoBehaviour
 {
     public int seedsCollected = 0;
 
+    [Header("Audio")]
+    public AudioSource pickupSound;
+    public AudioSource lightSound;
+
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Lol triggered" + other.gameObject.name);
+        Debug.Log("Lol triggered " + other.gameObject.name);
+
         if (other.CompareTag("Seed"))
         {
             seedsCollected++;
             Destroy(other.gameObject);
+            pickupSound?.Play();  // Play pickup sound
             Debug.Log("Seed collected! Total: " + seedsCollected);
         }
 
         if (other.CompareTag("Light") && seedsCollected > 0)
         {
-            LightController light = other.GetComponent<LightController>();
-
-            if (light == null)
-                light = other.GetComponentInParent<LightController>();
-
-            if (light == null)
-                light = other.GetComponentInChildren<LightController>();
+            LightController light = other.GetComponent<LightController>() 
+                                  ?? other.GetComponentInParent<LightController>() 
+                                  ?? other.GetComponentInChildren<LightController>();
 
             if (light != null && !light.isLit)
             {
                 light.isLit = true;
                 seedsCollected--;
+                lightSound?.Play();  // Play light-up sound
                 Debug.Log("Seed used to light a source! Seeds remaining: " + seedsCollected);
             }
             else 
@@ -36,6 +39,5 @@ public class SeedCollector : MonoBehaviour
                 Debug.Log(!light?.isLit ?? false ? "Not lit" : "It's lit");
             }
         }
-
     }
 }
